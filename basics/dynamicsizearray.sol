2cd3df298a73866a236a0f222854c7a5a01f57be
Pratik Shetty <74893718+draco1725@users.noreply.github.com>
2022-10-02 21:57:04 +0530

    Update dynamicsizearray.sol

--- basics/dynamicsizearray.sol
+++ basics/dynamicsizearray.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-//DSA: it has 2 method extra  push:pob 
+//DSA: it has 2 method extra  push:pop
+//here we not stating any boxes we can make or delete any vlaue we want 
+//in fixed size array we stated array  for eg: uint [5]  we stated here but in dynamic we didnt  
 //we can not make dynamic size array in memory
 contract dynamicSizearray{
 
